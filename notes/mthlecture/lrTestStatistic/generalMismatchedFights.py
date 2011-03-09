22616eddf2d4231c72395de8054a54454c2bc514
Mark T. Holder <mtholder@gmail.com>
2011-03-08 18:45:24 -0600

    corrected mismatchedFightsLike.py to agree with the form of the likelihood used in the notes that were posted, and wrote the generalized form of the code.  Still need to implement the parameteric bootstrapping in the general form.

--- notes/mthlecture/lrTestStatistic/generalMismatchedFights.py
+++ notes/mthlecture/lrTestStatistic/generalMismatchedFights.py
@@ -4,50 +4,82 @@ import sys
 import math
 import random
 
-def calc_prob_same(prob_strong, w):
-    assert (prob_strong >= 0.0)
-    assert (prob_strong <= 1.0)
-    assert (w >= .5)    
-    assert (w <= 1)
-    if (w == .5) or (prob_strong == 0.0) or (prob_strong == 1.0):
-        return 0.5
-    omp = 1.0 - prob_strong
-    prob_mismatch = 2*prob_strong*omp
-    two_ps = prob_mismatch*(w*w + (1-w)*(1-w)) + .5*(1 - prob_mismatch)
-    return two_ps/2.0
 
-def ln_likelihood(prob_strong, w, num_same, num_diff):
+# we are going to use a summary that is a pair of numbers for each pair of males
+#   it will represent: 
+#    (the number of bouts won by male 0, the number of bouts won by male 1)
+
+
+real_data = [ (0, 2),
+              (1, 1),
+              (1, 1),
+              (0, 2),
+              (2, 0),
+              (1, 1),
+              (0, 2),
+              (2, 0),
+              (1, 1),
+              (0, 2),
+            ]
+
+
+the_data = real_data
+
+
+def ln_likelihood(prob_strong, w):
+    global the_data
     # Return -inf if the parameters are out of range...
     if w != w or prob_strong != prob_strong:
         return float('-inf')
     if (w < .5) or (w > 1.0) or (prob_strong < 0.0) or (prob_strong > 1.0):
         return float('-inf')
-    # If the parameters are legal, then return the log-likelihood
-    prob_same = calc_prob_same(prob_strong, w)
-    prob_diff = .5 - prob_same
-    ln_prob_same = math.log(prob_same)
-    ln_prob_diff = math.log(prob_diff)
-    ln_l = num_same*ln_prob_same + num_diff*ln_prob_diff
+    
+    prob_SS = prob_strong*prob_strong
+    prob_SW = prob_strong*(1-prob_strong)
+    prob_WS = (1-prob_strong)*prob_strong
+    prob_WW = (1-prob_strong)*(1 - prob_strong)
+    
+    prob_even = prob_WW + prob_SS
+
+    prob0_even = 0.5
+    prob1_even = 1 - prob0_even
+
+    prob0_SW = w
+    prob1_SW = 1 - prob0_SW
+
+    prob0_WS = 1 - w
+    prob1_WS = 1 - prob0_WS
+
+    ln_l = 0.0
+    for datum in the_data:
+        num0_wins = datum[0]
+        num1_wins  = datum[1]
+        
+        prob_datum_if_even = (prob0_even**num0_wins) * (prob1_even**num1_wins)
+        prob_datum_if_SW = (prob0_SW**num0_wins) * (prob1_SW**num1_wins)
+        prob_datum_if_WS = (prob0_WS**num0_wins) * (prob1_WS**num1_wins)
+
+        prob_datum = (  prob_even*prob_datum_if_even
+                      + prob_SW*prob_datum_if_SW
+                      + prob_WS*prob_datum_if_WS )
+
+        if prob_datum <= 0.0:
+            return float('-inf')
+
+        ln_l = ln_l + math.log(prob_datum)
     return ln_l
 
 def scipy_ln_likelihood(x):
-    global num_same, num_diff
-    raw = ln_likelihood(x[0], x[1], num_same, num_diff)
+    raw = ln_likelihood(x[0], x[1])
     return -raw
 
 
-def estimate_global_MLE(s, d):
-    global num_same, num_diff
-    num_same = s
-    num_diff = d
+def estimate_global_MLE():
     x0 = [.75, .75]
     param_opt = optimize.fmin(scipy_ln_likelihood, x0, xtol=1e-8, disp=False)
     return param_opt[0], param_opt[1], -scipy_ln_likelihood(param_opt)
 
-def maximize_lnL_fixed_w(s, d, w):
-    global num_same, num_diff
-    num_same = s
-    num_diff = d
+def maximize_lnL_fixed_w(w):
     x0 = [.75]
     f = lambda x : scipy_ln_likelihood([x[0], w])
     param_opt = optimize.fmin(f, x0, xtol=1e-8, disp=False)
@@ -83,24 +115,12 @@ if __name__ == '__main__':
     # user-interface and sanity checking...
     print_estimates = False
     
-    num_same = int(sys.argv[1])
-    num_diff = int(sys.argv[2])
-    w_null = float(sys.argv[3])
-    num_sims = int(sys.argv[4])
-        
-    if num_diff < 0 or num_same < 0:
-        sys.exit("The number of bouts won by the same or different individuals must be non-negative")
-    
-    
+    w_null = float(sys.argv[1])
+    num_sims = int(sys.argv[2])
     
     # Calculate and report the MLEs and LRT...
-    
-    print
-    print num_same, 'trials in which the same individual wins both bouts.'
-    print num_diff, 'trials in which different individuals wins each bout.'
-    print
 
-    prob_strong_MLE, w_MLE, lnL = estimate_global_MLE(num_same, num_diff)
+    prob_strong_MLE, w_MLE, lnL = estimate_global_MLE()
 
     print "MLE of prob_strong =", prob_strong_MLE
     print "MLE of w =", w_MLE
@@ -108,7 +128,7 @@ if __name__ == '__main__':
     print "L at MLEs =", math.exp(lnL)
     print
 
-    prob_strong_null, lnL_null = maximize_lnL_fixed_w(num_same, num_diff, w_null)
+    prob_strong_null, lnL_null = maximize_lnL_fixed_w(w_null)
 
     print "MLE of prob_strong at null w =", prob_strong_null
     print "null of w =", w_null
@@ -126,7 +146,7 @@ if __name__ == '__main__':
     # Parametric bootstrapping to produce the null distribution of the LRT statistic
     if num_sims < 1:
         sys.exit(0)
-        
+    sys.exit("pboot not written")
     print "Generating null distribution of LRT..."
     n = num_same + num_diff
 
