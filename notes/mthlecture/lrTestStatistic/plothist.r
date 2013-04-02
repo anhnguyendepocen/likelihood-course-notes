f51018048a127c72c5b4c067bd3f8a8291f334de
Mark T. Holder <mtholder@gmail.com>
2013-04-02 15:07:28 -0500

    old changes

--- notes/mthlecture/lrTestStatistic/plothist.r
+++ notes/mthlecture/lrTestStatistic/plothist.r
@@ -1,4 +1,4 @@
 d <- read.table("pboot.txt", header=TRUE, sep="\t");
 pdf("lrtnull.pdf");
-hist(d$lrt, main="Null Distribution of LRT statistics");
+hist(d$lrt, main="Null Distribution of LRT statistics", xlab="P(x|y)");
 dev.off();
